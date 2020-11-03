a5e07fd542bd9e02bfce84c0205f6895c8ad840b
Your Name <you@eaxample.com>
2020-11-03 16:38:10 -0700

    implemented firebase utils, included ability to store authenticated users into our firestore db

--- src/App.js
+++ src/App.js
@@ -5,30 +5,46 @@ import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 
 class App extends React.Component {
-constructor() {
-  super();
+  constructor() {
+    super();
 
-  this.state = {
-    currentUser: null
+    this.state = {
+      currentUser: null,
+    };
   }
-}
 
-unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
-
-//Below is how we get the authentication to work using our firebase.auth() that we exported in the utils - this way we don't have to remount our entire app
+  //Below is how we get the authentication to work using our firebase.auth() that we exported in the utils - this way we don't have to remount our entire app
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              }
+            }
+          );
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+
+      // this.setState({ currentUser: user});
+      // console.log(user);
+    });
   }
-  
+
   //This is how we clsoe the subscription but I still don't understand what is happening here
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -36,19 +52,19 @@ unsubscribeFromAuth = null
 
   render() {
     return (
-      <div>   
-
-      {/* The currentUser prop will let the header know if the user is signed in or not */}
-        <Header currentUser={this.state.currentUser }/> {/*With Header placed here - It will show up on all pages below */}
-       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-       </Switch>
+      <div>
+        {/* The currentUser prop will let the header know if the user is signed in or not */}
+        <Header currentUser={this.state.currentUser} />{" "}
+        {/*With Header placed here - It will show up on all pages below */}
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
       </div>
     );
   }
-  }
+}
   
 
 export default App;
